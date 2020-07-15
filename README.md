d89843e53efeb215e7745a1f18d242be144cfb69
leoberniga <leo.berniga@gmail.com>
2020-07-14 23:39:41 -0400

    commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# frontendsp
+Este es un proyecto de prueba.
+
+## Contenido
+
+Las imágenes del slide son de contenido propio
+
+### Más info
+
+Sitio institucional para pizzerías.
+
+### Responsive
+
+El sitio está preparado para navegadores de todo tipo según formato estandar de Bootstrap
+
+### Paso a paso
+
+Primeramente se ha creado un archivo en create-react-app
+Luego instalé react-bootstrap
+Posteriormente trabajé carpeta por carpeta creando los módulos
+
---
+++ precache-manifest.9d220f59e5313dc0a5b6144d5f6430ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe71e085e6ea361b5fd9c3de45cf6d4",
+    "url": "/frontendsp/index.html"
+  },
+  {
+    "revision": "d6abf39b0e952f85ea7d",
+    "url": "/frontendsp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bc4f4fee7fc1cbfe2fb2",
+    "url": "/frontendsp/static/css/main.00d43f59.chunk.css"
+  },
+  {
+    "revision": "d6abf39b0e952f85ea7d",
+    "url": "/frontendsp/static/js/2.427e1f34.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/frontendsp/static/js/2.427e1f34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc4f4fee7fc1cbfe2fb2",
+    "url": "/frontendsp/static/js/main.772cb7fe.chunk.js"
+  },
+  {
+    "revision": "ba9ad42b9d53b6092fd9",
+    "url": "/frontendsp/static/js/runtime-main.6549b9e9.js"
+  }
+]);
